d03fda7f960f4e97106d505c21d27ce64040a34f
Conner Copeland <ccopelan@fredhutch.org>
2025-03-19 18:04:54 -0700

    Add flag for replicated samples to sample sheet generator

--- python/generate_sample_sheet.py
+++ python/generate_sample_sheet.py
@@ -35,9 +35,11 @@ def parse_args(sys_args: str) -> argparse.Namespace:
     )
 
     parser.add_argument(
-        "replicate_list",
+        "identifier_list",
         type=str,
-        help="Path to a text file with one replicate name per line."
+        help="Path to a text file with one sample or replicate identifier per line. If a sample is \
+            replicated, this should include a replicate identifier. For example, if sample 1234 \
+            has replicates A and B, then this file should include 1234_A and 1234_B."
     )
 
     parser.add_argument(
@@ -52,28 +54,42 @@ def parse_args(sys_args: str) -> argparse.Namespace:
         help="Path to output sample sheet in CSV format"
     )
 
+    parser.add_argument(
+        "--replicated",
+        action="store_true",
+        help="Set if input data has replicates. Default is False."
+    )
+
     return parser.parse_args()
 
 
 def _main():
     args = parse_args(sys.argv[1:])
-    replicate_list_path = args.replicate_list
+    identifier_list_path = args.identifier_list
     fastq_dir_path = args.fastq_dir
     output_path = args.output_sheet
+    has_replicates = args.replicated
 
-    replicate_list = []
+    file_ID_list = []
 
-    with open(replicate_list_path, "r", encoding="utf8") as replicate_file:
-        for replicate in replicate_file:
-            replicate_list.append(replicate.strip())
+    with open(identifier_list_path, "r", encoding="utf8") as identifier_file:
+        for file_ID in identifier_file:
+            file_ID_list.append(file_ID.strip())
 
     with open(output_path, "w", encoding="utf8") as sampleSheet:
         sampleSheet.write(f"sample,replicate_id,fastq1,fastq2\n")
 
-        for replicate in replicate_list:
-            # currently, this will only work for underscores. If need be, use re.split() to support multiple break characters
-            sample_id = replicate.split("_")[0]
-            sampleSheet.write(f"{sample_id}, {replicate}, {fastq_dir_path}/{replicate}_1.fastq.gz, {fastq_dir_path}/{replicate}_2.fastq.gz\n")
+        for file_ID in file_ID_list:
+            if has_replicates:
+                # in this case, there's a replicate ID we want to split off from sample ID
+                # currently, this will only work for underscores. If need be, use re.split() to support multiple break characters
+                sample_list = file_ID.split("_")[:-1]
+                sample = "_".join(sample_list)
+                sampleSheet.write(f"{sample}, {file_ID}, {fastq_dir_path}/{file_ID}_1.fastq.gz, {fastq_dir_path}/{file_ID}_2.fastq.gz\n")
+
+            else:
+                # in this case, sample ID and file ID are the same
+                sampleSheet.write(f"{file_ID}, {file_ID}, {fastq_dir_path}/{file_ID}_1.fastq.gz, {fastq_dir_path}/{file_ID}_2.fastq.gz\n")
     
 
 if __name__ == "__main__":
